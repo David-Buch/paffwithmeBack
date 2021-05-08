35f1d7026223fe7f54a90eb60b49fb3be42c6205
David-Buch <davidbuchgraber@gmail.com>
2021-05-08 11:01:40 +0200

    third Commit

--- config/UserDB.js
+++ config/UserDB.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'pipepwa',
+    host: 'eu-cdbr-west-01.cleardb.com',
+    user: 'b5357ab4b70e69',
+    password: '7e5d2003',
+    database: 'heroku_cf88fcb1628b75b',
 });
 
 module.exports = db;
+
