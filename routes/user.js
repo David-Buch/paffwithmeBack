41a1b6c159acb1a4fa326ce6eb469c3f33cdaf54
David-Buch <davidbuchgraber@gmail.com>
2021-05-22 12:41:35 +0200

    cookies

--- routes/user.js
+++ routes/user.js
@@ -16,7 +16,8 @@ router.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        secure: true
+        secure: true,
+        maxAge: 7 * 24 * 60 * 60 * 1000
     }
 }));
 router.use(cookieParser());
