12758b972b49845562dd99ab9f215654a80e8de3
David-Buch <davidbuchgraber@gmail.com>
2021-06-29 17:09:42 +0200

    storie route

--- routes/stories.js
+++ routes/stories.js
@@ -2,25 +2,77 @@ const express = require('express');
 require('dotenv').config();
 const router = express.Router();
 const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
+
+let timeOuts = [];
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'uploads/');
+        cb(null, './uploads');
     },
 
     filename: function (req, file, cb) {
-        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+        cb(null, file.originalname + '-' + Date.now());
     }
 });
-
-var upload = multer({ storage: storage })
-
+const upload = multer({ storage: storage })
 router.use(express.json());
 
 router.post('/upload', upload.single('storie'), (req, res) => {
-    const { filename: image } = req.file;
-    const username = req.body.name;
+    console.log(req.file);
+
+    //deleting Stories  
+    const timer = setTimeout(() => deleteFile(req.file.filename),
+        24 * 60 * 60 * 1000)
+    timeOuts.push(timer);
 
     res.send('worked');
 });
+
+router.post('/get', (req, res) => {
+    //working on it next time
+    const username = req.body.username;
+    const directoryPath = path.join(__dirname, '../', 'uploads'); //err C:\Users\David\paffwithme\server\routes\uploads
+
+    fs.readdir(directoryPath, function (err, files) {
+        //handling error
+        if (err) {
+            return console.log('Unable to scan directory: ' + err);
+        }
+        //listing all files using forEach
+        files.forEach(function (file) {
+            if (username == file.split('-')[0]) {
+                var options = {
+                    root: directoryPath
+                };
+                res.sendFile(file, options, function (err) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log('Sent:', file);
+                    }
+                });
+                //res.download(filePath);
+            }
+            else {
+                res.send({ success: false, message: 'No Storie from this User!' })
+            }
+
+        });
+    });
+
+    //send files
+    function sendFiles() {
+        let filePath = path.join(__dirname, "youe-file.whatever");
+        res.download(filePath);
+    }
+});
+
+const deleteFile = (filename) => {
+    fs.unlink("./uploads/" + filename, (err => {
+        if (err) throw err;
+    }));
+    console.log('delete');
+}
 
 module.exports = router;
